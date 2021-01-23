ad2895adf6d48010bc4c4cc44faaf1ddadaff80f
Jesse Tsang <1919892+JesseTsang@users.noreply.github.com>
2021-01-23 18:32:12 -0500

    init repo

---
+++ src/main/java/com/airgraftcodingchallenge/autocomplete_api/AutocompleteApiApplication.java
@@ -0,0 +1,13 @@
+package com.airgraftcodingchallenge.autocomplete_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airgraftcodingchallenge/autocomplete_api/AutocompleteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airgraftcodingchallenge.autocomplete_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
